05461a7a4dcbcb27419df0843784edaaadf95548
victortecbr <victor.tecbr@gmail.com>
2026-02-06 12:09:58 -0300

    nome idade cidade

---
+++ ad01.py
@@ -0,0 +1,7 @@
+# 1.Solicite nome, idade e cidade do usuário e exiba uma frase completa usando concatenação.
+
+nome = input("Qual o seu nome?")
+idade = input("Qual a sua idade?")
+cidade = input("Qual o seu cidade?")
+
+print(f"seu nome é {nome}, sua idade é {idade} e sua cidade é {cidade}.")
