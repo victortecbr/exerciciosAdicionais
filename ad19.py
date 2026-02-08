84a2c1ba323e5ac4f449893cebbfb5aaea461045
victortecbr <victor.tecbr@gmail.com>
2026-02-08 11:14:32 -0300

    exercícios adicionais além das aulas

---
+++ ad19.py
@@ -0,0 +1,12 @@
+# dividir com função
+
+def div (a,b,c):
+    return a / b * c + 200
+
+n1 = int(input("Número o 1: "))
+n2 = int(input("Número o 2: "))
+n3 = int(input("Número o 3: "))
+
+
+resultado = div (n1,n2,n3)
+print(resultado)
